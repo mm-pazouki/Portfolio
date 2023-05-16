06e5433a32887014b33447743111de201914c578
hafed <hafedrs@gmail.com>
2023-05-16 17:15:04 +0200

    popup JS

--- index.js
+++ index.js
@@ -49,6 +49,10 @@ buttonArr.map((el) => {
     popupObj.paragraph = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`;
 
     const closeButton = document.createElement('span');
+    closeButton.classList.add('closeMenu');
+    closeButton.addEventListener('click', () => {
+      popupContainer.remove();
+    })
     popup.appendChild(closeButton);
 
     const title = document.createElement('h2');
@@ -80,7 +84,8 @@ buttonArr.map((el) => {
 
     const tags = document.createElement('ul');
     popupObj.tags.map((t) => {
-      const tag = document.createElement('li');
+    const tag = document.createElement('li');
+      tag.classList.add('tag');
       tag.innerHTML = t;
       tags.appendChild(tag);
     });

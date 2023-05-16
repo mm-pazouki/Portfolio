c98c825eca8931d5462bd1c64fcf6dd953f257db
Mohammad Mehdi Pazouki <mm_pazouki@yahoo.com>
2023-05-16 20:06:14 +0330

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const projects = [
     role: 'Full Stack Developer',
     year: '2018',
     tags: ['html', 'css', 'javaScript', 'bootstrap'],
-    liveLink: 'https://pazouki.netlify.app/,
+    liveLink: 'https://pazouki.netlify.app/',
     sourceLink: 'https://github.com/mm-pazouki/Portfolio',
     paragraph:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
